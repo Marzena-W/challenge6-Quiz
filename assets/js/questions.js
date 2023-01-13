eee0d318e791a38f27697e3bef52b41a2fb510d8
Marzena-W <marzena.m.wilk@gmail.com>
2023-01-13 18:44:29 +0000

    added javascript files. One with questions

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        question: "What is the correct way to declare a new variable that can be changed?",
+        answers: [
+            "1. name = 'Ada",
+            "2. const name = 'Ada",
+            "3. let name = 'Ada",
+            "4. let name: 'Ada"
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above"
+        ]
+    }
+    {
+        question: "A tool used during development and debugging for printing content to the console is:",
+        answers: [
+            "1. JavaScript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console.log"
+        ]
+    }
+];
