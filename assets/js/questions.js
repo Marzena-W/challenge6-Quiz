102c52889ffdea228b08b2deecec17a6bb674592
Marzena-W <marzena.m.wilk@gmail.com>
2023-01-13 19:13:40 +0000

    added 5 questions to the quiz|

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,13 @@
-var questions = [
+var quizQuestions = [
     {
         question: "What is the correct way to declare a new variable that can be changed?",
         answers: [
             "1. name = 'Ada",
-            "2. const name = 'Ada",
-            "3. let name = 'Ada",
-            "4. let name: 'Ada"
-        ]
+            "2. let name: 'Ada",
+            "3. const name = 'Ada",
+            "4. let name = 'Ada"
+        ],
+        correctAnswerIndex: 2
     },
     {
         question: "Arrays in JavaScript can be used to store ____.",
@@ -15,15 +16,37 @@ var questions = [
             "2. other arrays",
             "3. booleans",
             "4. all of the above"
-        ]
-    }
+        ],
+        correctAnswerIndex: 4
+    },
     {
         question: "A tool used during development and debugging for printing content to the console is:",
         answers: [
             "1. JavaScript",
             "2. terminal/bash",
-            "3. for loops",
-            "4. console.log"
-        ]
-    }
+            "3. console.log",
+            "4. for loops"
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            "1. function myFunction()",
+            "2. function:myFunction()",
+            "3. function = myFunction()",
+            "4. none of the above"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            "1. <JavaScript>",
+            "2. <scripting>",
+            "3. <js>",
+            "4. <script>"
+        ],
+        correctAnswerIndex: 4
+    },
 ];
